0c5adf8572eeecfd4be6f31379c9502cf117272b
Smith Ringtho <sringtho@gmail.com>
2023-05-24 22:12:03 +0300

    Added the homepage video

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import GoogleLogin from 'react-google-login'
+import { FcGoogle } from 'react-icons/fc'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className=''>
+      <div className=''>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logowhite.png'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='flex flex-col items-center justify-start h-screen'>
+      <div className='relative h-full w-full'>
+        <video 
+            src={shareVideo}
+            type='video/mp4'
+            loop
+            autoPlay
+            controls={false}
+            muted
+            className='w-full h-full object-cover'
+        />
+      </div>
     </div>
   )
 }
