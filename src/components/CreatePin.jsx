100cc2aa6f05e8019f126a25536f23d3ec7bf09a
Smith Ringtho <sringtho@gmail.com>
2023-05-28 21:02:48 +0300

    Added the Navigation bar component

---
+++ src/components/CreatePin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreatePin = () => {
+  return (
+    <div>
+      Create Pin
+    </div>
+  )
+}
+
+export default CreatePin
+ 
---
+++ src/components/PinDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PinDetail = () => {
+  return (
+    <div>
+      Pin Detail
+    </div>
+  )
+}
+
+export default PinDetail
