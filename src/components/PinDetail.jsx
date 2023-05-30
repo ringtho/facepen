1b611f9c6e891c518bd2051ff744dfe806cb59bf
Smith Ringtho <sringtho@gmail.com>
2023-05-30 11:11:17 +0300

    Added absolute routing for the user profile to fix bugs

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -100,7 +100,7 @@ const PinDetail = ({ user }) => {
           </h1>
           <p className='mt-3'>{pinDetail.about}</p>
         </div>
-        <Link to={`user-profile/${pinDetail.postedBy?._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg">
+        <Link to={`/user-profile/${pinDetail.postedBy?._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg">
             <img 
                 className='w-8 h-8 rounded-full object-cover'
                 src={pinDetail.postedBy?.image}
@@ -125,7 +125,7 @@ const PinDetail = ({ user }) => {
           ))}
         </div>
             <div className='flex flex-wrap mt-6 gap-3'>
-              <Link to={`user-profile/${pinDetail.postedBy?._id}`}>
+              <Link to={`/user-profile/${pinDetail.postedBy?._id}`}>
                 <img 
                     className='w-10 h-10 rounded-full cursor-pointer'
                     src={pinDetail.postedBy?.image}
