e04e32a773d198675dc5aa565ca8df2e6f329bf5
Smith Ringtho <sringtho@gmail.com>
2023-05-30 10:42:30 +0300

    Fixed the routing for user profile

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -58,7 +58,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
                     <div className='flex items-center justify-between'>
                         <div className='flex gap-2'>
                             <a
-                                href={`${image?.asset?.url}?dl=`}
+                                href={`/${image?.asset?.url}?dl=`}
                                 download
                                 onClick={(e) => e.stopPropagation()}
                                 className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
